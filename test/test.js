34c29fbc256b87b21f4caaf475544b2b3f2edd0d
otodockal <oto.dockal@gmail.com>
2014-05-03 13:45:11 +0200

    test: add more tests

--- test/test.js
+++ test/test.js
@@ -1,22 +1,40 @@
-
 var Lab = require('lab');
-
-var plugin = require('./../index');
+var Gutil = require('gulp-util');
+var Glab = require('./../index');
 
 var expect = Lab.expect;
 var describe = Lab.experiment;
 var it = Lab.test;
 
-
 describe('index', function () {
 
-  it('should return stream object', function (done) {
+  it('should return stream', function (done) {
 
-    var stream = plugin();
+    var stream = Glab();
 
-    expect(typeof stream).to.equal('object');
-    expect(typeof stream.pipe).to.equal('function');
+    expect(Gutil.isStream(stream)).to.equal(true);
 
     done();
   });
+
+  it('should run truthy test by gulp-lab module with String options', function (done) {
+
+    var stream = Glab('-v -l');
+
+    stream.write(new Gutil.File({path: './test/truthy.js'}));
+    stream.end();
+
+    done();
+  });
+
+  it('should run truthy test by gulp-lab module with Array options', function (done) {
+
+    var stream = Glab(['-s', '-l']);
+
+    stream.write(new Gutil.File({path: './test/truthy.js'}));
+    stream.end();
+
+    done();
+  });
+
 });
---
+++ test/truthy.js
@@ -0,0 +1,14 @@
+var Lab = require('lab');
+
+var it = Lab.test;
+var expect = Lab.expect;
+
+
+it('should pass test', function (done) {
+
+  console.log('\n\n');
+  
+  expect(true).equal(true);
+
+  done();
+});
