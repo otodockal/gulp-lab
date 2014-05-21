3bbaaf954c501cac41ce6a6d872067b70e16d6fc
Damien <damien.feugas@gmail.com>
2014-05-21 09:16:55 +0200

    fix issue https://github.com/otodockal/gulp-lab/issues/2
    Make gulp lab usable on window platforms

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var Spawn = require('child_process').spawn;
 var Through2 = require('through2');
-
+var join = require('path').join;
 
 module.exports = function (options) {
 
   var paths = [];
-  var args = [process.env.PWD + '/node_modules/.bin/lab'];
+  // we expect that lab was declared as a project dependencies, and run directly its main script
+  // this way, it can be executed on every platform.
+  var args = [join(process.cwd(), 'node_modules', 'lab', 'bin', 'lab')];
 
   return Through2.obj(function (file, enc, cb) {
 
