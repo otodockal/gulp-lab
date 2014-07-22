5606f705956f34a3e7a5ea1b321e39e8f29e2bf6
otodockal <oto.dockal@gmail.com>
2014-07-22 23:11:26 +0200

    feat: add more tests

--- test/test.js
+++ test/test.js
@@ -26,23 +26,125 @@ describe('index', function () {
     stream.end(new Gutil.File({path: './test/truthy.js'}));
   });
 
-  it('should run truthy test by gulp-lab module with Array options', function (done) {
+  it('should emit an error if options object is passed with missing opt property', function (done) {
 
-    var stream = Glab(['-s', '-l']);
+    var failure;
+    var stream = Glab({
+      cmd: '-s -l'
+    });
 
-    stream.pipe(es.wait(done));
-    stream.end(new Gutil.File({path: './test/truthy.js'}));
+    stream.once('error', function (error) { failure = error; });
+    stream.pipe(es.wait(function () {
+      expect(failure, 'no error').to.be.an.instanceOf(Error);
+      expect(failure.message, 'message').to.match(/Object property "opt" must be an object!/i);
+      done();
+    }));
+    stream.end();
   });
 
-  it('should emit an error if the tests fail', function (done) {
+  it('should emit an error if options object is passed with missing emitLabError property', function (done) {
 
-    var stream = Glab('-s -l');
     var failure;
+    var stream = Glab({
+      cmd: '-s -l',
+      opt: {}
+    });
+
+    stream.once('error', function (error) { failure = error; });
+    stream.pipe(es.wait(function () {
+      expect(failure, 'no error').to.be.an.instanceOf(Error);
+      expect(failure.message, 'message').to.match(/Object property "emitLabError" must be a boolen!/i);
+      done();
+    }));
+    stream.end();
+  });
+
+  it('should emit an error if options object is passed with NON boolean emitLabError property', function (done) {
+
+    var failure;
+    var stream = Glab({
+      cmd: '-s -l',
+      opt: {
+        emitLabError: 'true'
+      }
+    });
+
+    stream.once('error', function (error) { failure = error; });
+    stream.pipe(es.wait(function () {
+      expect(failure, 'no error').to.be.an.instanceOf(Error);
+      expect(failure.message, 'message').to.match(/Object property "emitLabError" must be a boolen!/i);
+      done();
+    }));
+    stream.end();
+  });
+
+  it('should emit an error if the test fail', function (done) {
+
+    var failure;
+    var stream = Glab({
+      cmd: '-s -l',
+      opt: {
+        emitLabError: true
+      }
+    });
 
     stream.once('error', function (error) { failure = error; });
     stream.pipe(es.wait(function () {
       expect(failure, 'no error').to.be.an.instanceOf(Error);
       expect(failure.message, 'message').to.match(/exited with errors/i);
+      done();
+    }));
+    stream.end(new Gutil.File({path: './test/fail.js'}));
+  });
+
+  it('should NOT emit an error if the test fail', function (done) {
+
+    var failure;
+    var stream = Glab({
+      cmd: '-s -l',
+      opt: {
+        emitLabError: false
+      }
+    });
+
+    stream.once('error', function (error) { failure = error; });
+    stream.pipe(es.wait(function () {
+      expect(failure).to.equal(undefined);
+      done();
+    }));
+    stream.end(new Gutil.File({path: './test/fail.js'}));
+  });
+
+  it('should emit an error if the test fail - missing cmd', function (done) {
+
+    var failure;
+    var stream = Glab({
+      opt: {
+        emitLabError: true
+      }
+    });
+
+    stream.once('error', function (error) { failure = error; });
+    stream.pipe(es.wait(function () {
+      expect(failure, 'no error').to.be.an.instanceOf(Error);
+      expect(failure.message, 'message').to.match(/exited with errors/i);
+      done();
+    }));
+    stream.end(new Gutil.File({path: './test/fail.js'}));
+  });
+
+  it('should NOT emit an error if the test fail - missing cmd', function (done) {
+
+    var failure;
+    var stream = Glab({
+      opt: {
+        emitLabError: false
+      }
+    });
+
+    stream.once('error', function (error) { failure = error; });
+    stream.pipe(es.wait(function () {
+      expect(failure).to.equal(undefined);
       done();
     }));
     stream.end(new Gutil.File({path: './test/fail.js'}));
